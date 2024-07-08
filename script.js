9912aa4363255da6b26bd0a4ad73f7eb94c4ab11
George-Oladokun <oladokungeorge8@gmail.com>
2024-07-08 13:01:51 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer", "Backend Developer", "Youtuber" , "Web Developer", "Graphics Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
